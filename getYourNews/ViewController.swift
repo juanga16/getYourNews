a655b2a21b409f59becb54f8d19a8f43dc88194e
Juanga <juanga141280@gmail.com>
2019-10-14 06:40:32 -0700

    Initial Commit

---
+++ getYourNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getYourNews
+//
+//  Created by Cosme Fulanito on 14/10/2019.
+//  Copyright © 2019 Cosme Fulanito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
